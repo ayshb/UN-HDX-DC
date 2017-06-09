2c95459d82ae9269ea7f11ca2325c7a13e4f1786
Ash <aybala@deloitte.com>
2017-06-09 15:43:41 -0400

    final

--- js/scripts.js
+++ js/scripts.js
@@ -2,41 +2,6 @@
 console.log(survey_data);
 var survey = TAFFY(survey_data);
 
-var canvas = document.getElementById('myChart');
-var data = {
-    labels: ["January", "February", "March", "April", "May", "June", "July"],
-    datasets: [{
-        label: "My First dataset",
-        backgroundColor: "rgba(255,99,132,0.2)",
-        borderColor: "rgba(255,99,132,1)",
-        borderWidth: 2,
-        hoverBackgroundColor: "rgba(255,99,132,0.4)",
-        hoverBorderColor: "rgba(255,99,132,1)",
-        data: [65, 59, 20, 81, 56, 55, 40],
-    }]
-};
-var option = {
-    scales: {
-        yAxes: [{
-            stacked: true,
-            gridLines: {
-                display: true,
-                color: "rgba(255,99,132,0.2)"
-            }
-        }],
-        xAxes: [{
-            gridLines: {
-                display: false
-            }
-        }]
-    }
-};
-
-var myBarChart = Chart.Bar(canvas, {
-    data: data,
-    options: option
-});
-
 //choropleth
 var width = 900,
     height = 500;
